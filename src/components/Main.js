57985758e88169c97f8dad4f3b046c4c83c474e8
Akhtool <Zelimhan1997@mail.ru>
2023-04-24 19:35:28 +0300

    feat: добавить обработчики кликов на попапы

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,28 @@
 function Main() {
+  function handleEditAvatarClick() {
+    document
+      .querySelector(".popup_type_edit-avatar")
+      .classList.add("popup_opened");
+  }
+
+  function handleEditProfileClick() {
+    document
+      .querySelector(".popup_type_profile-edit")
+      .classList.add("popup_opened");
+  }
+
+  function handleAddPlaceClick() {
+    document
+      .querySelector(".popup_type_add-card")
+      .classList.add("popup_opened");
+  }
   return (
     <main className="container">
       <section className="profile">
-        <button className="profile__image-edit-button">
+        <button
+          className="profile__image-edit-button"
+          onClick={handleEditAvatarClick}
+        >
           <img src="#" alt="#" className="profile__image" />
         </button>
         <div className="profile__info">
@@ -12,6 +32,7 @@ function Main() {
               type="button"
               className="profile__edit-button"
               aria-label="edit"
+              onClick={handleEditProfileClick}
             ></button>
           </div>
           <p className="profile__description"></p>
@@ -20,6 +41,7 @@ function Main() {
           type="button"
           className="profile__add-button"
           aria-label="add"
+          onClick={handleAddPlaceClick}
         ></button>
       </section>
       <section className="cards">
